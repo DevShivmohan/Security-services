d18798c8b110226adb74ad99f2977f2c1d31030b
Shivmohan <nshivmohan08@gmail.com>
2022-06-30 14:20:03 +0530

    added microservices configuration

--- TX-RX service/security-server/src/main/java/com/shiv/security/SecurityDemoApplication.java
+++ TX-RX service/security-server/src/main/java/com/shiv/security/SecurityDemoApplication.java
@@ -2,8 +2,10 @@ package com.shiv.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SecurityDemoApplication {
 	public static void main(String[] args) {
 		String uuid="609ddfbd-5a5d-4503-b515-05c0f4e7936b__";
