d6d66a81eb8d6f0540b4a2e79a5a9674748576e9
shivmohan <nshivmohan08@gmail.com>
2022-12-25 01:10:39 +0530

    Added CI/CD for

--- security-server/src/main/java/com/shiv/security/constant/ApiConstant.java
+++ security-server/src/main/java/com/shiv/security/constant/ApiConstant.java
@@ -6,7 +6,7 @@ import java.io.File;
 
 @Slf4j
 public class ApiConstant {
-    public static final String SERVER_DOWNLOAD_DIR=System.getProperty("user.dir")+File.separator+"server"+File.separator+"download";
+    public static final String SERVER_DOWNLOAD_DIR="/home/send-anywhere"+File.separator+"server"+File.separator+"download";
     static {
         if(new File(SERVER_DOWNLOAD_DIR).mkdirs())
             log.info("Server directory created");
